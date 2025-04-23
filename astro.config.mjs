c0bf01ac0fc4cc408a1460a7476b065746bea150
Emperor of Obsidian <obsidianempireaoc@gmail.com>
2025-04-23 06:08:11 -0400

    updated to work on go daddy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: '/oe-aoc/',
-    site: 'https://obsidianempire.github.io/oe-aoc/',
-  });
+  base: '/', // This is correct for a custom root domain
+});
