b60c70ebbe2495fbb3fb58aaa424bdc7d831f076
Emperor of Obsidian <obsidianempireaoc@gmail.com>
2025-04-22 22:50:28 -0400

    git deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    base: '/oe-aoc/',
+    site: 'https://obsidianempire.github.io/oe-aoc/',
+  });
